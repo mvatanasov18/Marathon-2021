97652f62aac57aaf9671a62555862b0cb0922e3f
PIPetkova19 <PIPetkova19@codingburgas.bg>
2021-06-13 17:24:15 +0300

    Add js for navbar

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+/* Add or remove the "responsive" class  */
+function myFunction() {
+    var x = document.getElementById("topnav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
